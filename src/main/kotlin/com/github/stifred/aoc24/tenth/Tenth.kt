39386a81acf269c5fcebb6f3de8da289eeb304c9
Stian Fredrik Aune <post@stian-aune.com>
2024-12-10 18:02:26 +0100

    kinda pointless

--- src/main/kotlin/com/github/stifred/aoc24/tenth/Tenth.kt
+++ src/main/kotlin/com/github/stifred/aoc24/tenth/Tenth.kt
@@ -12,24 +12,20 @@ val tenth = solution(day = 10) {
 }
 
 data class TrailMap(private val chars: List<Char>, private val width: Int) {
-  private val height = chars.size / width
+  private val trailHeads = chars.withIndex().filter { (_, c) -> c == START }
 
   val score get(): Int = trailHeads.sumOf { (i, c) -> scan(i.toPos(), c).distinct().count() }
   val sumOfRatings get(): Int = trailHeads.sumOf { (i, c) -> scan(i.toPos(), c).count() }
 
-  private val trailHeads get() = chars.withIndex().asSequence().filter { (_, c) -> c == START }
-
   private fun scan(position: Position, value: Char): Sequence<Position> =
-    position.toIndexOrNull()?.let { index ->
-      when {
-        chars[index] != value -> sequenceOf()
-        value == END -> sequenceOf(position)
-        else -> Direction.nonDiagonals.asSequence().flatMap { scan(position.move(it), value + 1) }
-      }
-    } ?: sequenceOf()
+    when {
+      chars[position.toIndex()] != value -> sequenceOf()
+      value == END -> sequenceOf(position)
+      else -> Direction.nonDiagonals.asSequence().flatMap { scan(position.move(it), value + 1) }
+    }
 
   private fun Int.toPos() = Position(this % width, this / width)
-  private fun Position.toIndexOrNull() = if (x in 0 until width && y in 0 until height) ((y * width) + x) else null
+  private fun Position.toIndex() = (y * width) + x
 
   companion object {
     const val START = '0'
@@ -37,7 +33,20 @@ data class TrailMap(private val chars: List<Char>, private val width: Int) {
   }
 }
 
-fun String.toTrailMap() = TrailMap(
-  chars = filter { it != '\n' }.toList(),
-  width = indexOf('\n'),
-)
+fun String.toTrailMap(): TrailMap {
+  val width = indexOf('\n') + 2
+  val chars = this
+
+  return TrailMap(
+    chars = buildList {
+      repeat(width) { add('.') }
+      for (char in chars) {
+        if (char == '\n') {
+          repeat(2) { add('.') }
+        } else add(char)
+      }
+      repeat(width) { add('.') }
+    },
+    width = width,
+  )
+}
