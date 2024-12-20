3aaf5f3882a77d12566b01965add91b451fe28cd
Stian Fredrik Aune <post@stian-aune.com>
2024-12-20 13:07:32 +0100

    Optimised

--- src/main/kotlin/com/github/stifred/aoc24/day20/Day20.kt
+++ src/main/kotlin/com/github/stifred/aoc24/day20/Day20.kt
@@ -4,13 +4,15 @@ import com.github.stifred.aoc24.day16.DijkstraMaze
 import com.github.stifred.aoc24.day16.asDijkstraMaze
 import com.github.stifred.aoc24.shared.Direction
 import com.github.stifred.aoc24.shared.Position
+import com.github.stifred.aoc24.shared.search.SearchStateWithKey
+import com.github.stifred.aoc24.shared.search.dfs
 import com.github.stifred.aoc24.shared.solution
 
 val day20 = solution(day = 20) {
   val track = parseInput { it.asDijkstraMaze().asRacetrack() }
 
-  part1 { track.cheats(2, 100).distinct().count() }
-  part2 { track.cheats(20, 100).distinct().count() }
+  part1 { track.cheats(2, 100).count() }
+  part2 { track.cheats(20, 100).count() }
 }
 
 data class Racetrack(val steps: List<Position>) {
@@ -18,9 +20,9 @@ data class Racetrack(val steps: List<Position>) {
   val end = steps.last()
 
   fun cheats(length: Int, threshold: Int) = sequence {
-    for ((fromPico, from) in steps.withIndex()) {
-      for ((i, to) in steps.subList(fromPico + 1, steps.size).withIndex()) {
-        val deltaPico = i + 1
+    for ((fromPico, from) in steps.subList(0, steps.size - threshold).withIndex()) {
+      for ((i, to) in steps.subList(fromPico + threshold, steps.size).withIndex()) {
+        val deltaPico = i + threshold
         val md = to manhattanTo from
         if (md > length || md > deltaPico - threshold) continue
 
@@ -32,17 +34,24 @@ data class Racetrack(val steps: List<Position>) {
 
 data class Cheat(val start: Position, val end: Position, val saving: Int)
 
-fun DijkstraMaze.asRacetrack() = buildList {
-  var current = start
-  add(current)
-  do {
+fun DijkstraMaze.asRacetrack(): Racetrack {
+  data class Path(val positions: List<Position>): SearchStateWithKey<Position> {
+    override val key get() = positions.last()
+  }
+
+  val dfs = dfs(Path::class).apply { push(Path(listOf(start))) }
+  val path = dfs.find { path ->
+    val current = path.key
+    if (current == end) found(path)
+
     for (dir in Direction.nonDiagonals) {
       val next = current.move(dir)
-      if (next in vertices && next !in this) {
-        current = next
+      if (next in vertices && next !in path.positions) {
+        continueWith(Path(positions = path.positions + next))
         break
       }
     }
-    add(current)
-  } while (current != end)
-}.let { Racetrack(it) }
+  } ?: error("Path not found")
+
+  return Racetrack(path.positions)
+}
