153a50f0b05790599b1fce520dd2d78b1a30c8d2
Stian Fredrik Aune <post@stian-aune.com>
2024-12-20 13:33:09 +0100

    More silly optimisations.

--- src/main/kotlin/com/github/stifred/aoc24/day19/Day19.kt
+++ src/main/kotlin/com/github/stifred/aoc24/day19/Day19.kt
@@ -9,24 +9,43 @@ val day19 = solution(day = 19) {
   part2 { layout.countAllCombos() }
 }
 
-data class LinenLayout(val towelPatterns: Set<String>, val designs: List<String>) {
-  fun countPossibleDesigns(): Int = designs.count { it.possibleCombos(towelPatterns) > 0 }
-  fun countAllCombos(): Long = designs.sumOf { it.possibleCombos(towelPatterns) }
-}
+enum class Color { White, Blue, Black, Red, Green }
 
-fun String.possibleCombos(
-  patterns: Set<String>,
-  cache: MemoizationCache<String, Long> = MemoizationCache(),
-): Long = if (isEmpty()) 1L else cache.runMemoized(this) {
-  patterns.asSequence()
-    .filter { startsWith(it) }
-    .map { substring(it.length) }
-    .sumOf { it.possibleCombos(patterns, cache) }
+typealias Pattern = List<Color>
+typealias Design = List<Color>
+
+data class LinenLayout(val patterns: Set<Pattern>, val designs: List<Design>) {
+  fun countPossibleDesigns(): Int = possibleCombos().count { it > 0 }
+  fun countAllCombos(): Long = possibleCombos().sum()
+
+  private fun possibleCombos() = MemoizationCache<Design, Long>().let { cache ->
+    designs.map { it.possibleCombos(cache) }
+  }
+
+  private fun Design.possibleCombos(
+    cache: MemoizationCache<Design, Long>,
+  ): Long = if (isEmpty()) 1L else cache.runMemoized(this) {
+    patterns.asSequence()
+      .filter { it.size <= size }
+      .filter { subList(0, it.size) == it }
+      .map { subList(it.size, size) }
+      .sumOf { it.possibleCombos(cache) }
+  }
 }
 
 fun String.asLinenLayout() = split("\n\n").let { (towelStr, designStr) ->
   LinenLayout(
-    towelPatterns = towelStr.split(", ").toSet(),
-    designs = designStr.split('\n'),
+    patterns = towelStr.split(", ").map(String::asColors).toSet(),
+    designs = designStr.split('\n').map(String::asColors),
   )
 }
+
+private fun String.asColors(): List<Color> = map { CHAR_MAP.getValue(it) }
+
+private val CHAR_MAP = mapOf(
+  'w' to Color.White,
+  'u' to Color.Blue,
+  'b' to Color.Black,
+  'r' to Color.Red,
+  'g' to Color.Green,
+)
